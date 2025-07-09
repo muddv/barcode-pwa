81098228370353b8dd60dfdb54afd46d5789f58d
Misha Dedov <test@test.test>
2025-07-09 18:46:41 +0300

    refactor

--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,43 @@
 import './style.css'
 import { initPWA } from './pwa.ts'
+
 let paused = false
-async function main() {
+let camera: MediaStream
+let barcodeDetectionTick = 600
 
+function initHTML() {
   const app = document.querySelector<HTMLDivElement>('#app')!
-
   app.innerHTML =
-    `
-  <div id='container'>
+    `<div id='container'>
       <canvas id="barcodeImg"></canvas>
       <button id="vidc">play/pause</button>
-
       <button id="detect">detect barcode</button>
-<label>
-from video
-      <input id="from" type="checkbox" />
-</label>
-  <div id='value'></div>
-
+      <label>
+        from video
+        <input id="from" type="checkbox" />
+      </label>
+      <div id='value'></div>
       <video id="vid"></video>
-  <div
-    id="pwa-toast"
-    role="alert"
-    aria-labelledby="toast-message"
-  >
-    <div class="message">
-      <span id="toast-message"></span>
-    </div>
-    <div class="buttons">
-        <button id="pwa-refresh" type="button">
-          Reload
-        </button>
-        <button id="pwa-close" type="button">
-          Close
-        </button>
-    </div>
-  </div>
+      <div
+        id="pwa-toast"
+        role="alert"
+        aria-labelledby="toast-message"
+      >
+        <div class="message">
+          <span id="toast-message"></span>
+        </div>
+        <div class="buttons">
+            <button id="pwa-refresh" type="button">
+              Reload
+            </button>
+            <button id="pwa-close" type="button">
+              Close
+            </button>
+        </div>
+      </div>
 `
-
-
-
-  let camera: MediaStream
+}
+async function initCamera() {
   try {
     camera = await navigator.mediaDevices.getUserMedia({
       video: {
@@ -58,12 +55,31 @@ from video
       console.error(e)
     }
   }
+}
+
+
+function drawVideoOnCanvas(width = 500, height = 500) {
+  const canvas: HTMLCanvasElement = document.querySelector('#barcodeImg')!
+  const video: HTMLVideoElement = document.querySelector('#vid')!
+  const context = canvas.getContext("2d")!
+  canvas.width = width
+  canvas.height = height
+  const drawLoop = () => {
+    context.drawImage(video, 0, 0, width, height)
+    if (!paused) {
+      requestAnimationFrame(drawLoop)
+    }
+  }
+  requestAnimationFrame(drawLoop)
+}
+
+function initVideoElement() {
   const videoElement = document.querySelector<HTMLVideoElement>("#vid")!
   videoElement.srcObject = camera!
   const videoControlButton = document.querySelector<HTMLDivElement>('#vidc')!
 
   void videoElement.play()
-  let isPlaying = true
+  let isPlaying = false
   videoControlButton.addEventListener('click', () => {
     if (paused === true) {
       paused = false
@@ -79,104 +95,89 @@ from video
       void videoElement.play()
     }
   })
-
-
-  function detectBarcode(fromImage = true) {
-    let imageEl: HTMLCanvasElement = document.querySelector("#barcodeImg")!
-    if (!fromImage) {
-      imageEl = document.querySelector("#vid")!
-    }
-
-    // check compatibility
-    if (!("BarcodeDetector" in globalThis)) {
-      console.log("Barcode Detector is not supported by this browser.")
-    } else {
-      // create new detector
-      // @ts-ignore
-      const barcodeDetector = new BarcodeDetector({
-        formats: [
-          "aztec",
-          "code_128",
-          "code_39",
-          "code_93",
-          "codabar",
-          "data_matrix",
-          "ean_13",
-          "ean_8",
-          "itf",
-          "pdf417",
-          "qr_code",
-          "upc_a",
-          "upc_e",
-          "unknown"
-        ],
-      });
-      barcodeDetector
-        .detect(imageEl)
-        .then((barcodes: any) => {
-          barcodes.forEach((barcode: any) => {
-            onBarcodeDetected(barcode)
-          })
-        })
-        .catch((err: any) => {
-          console.log(err)
-        });
-    }
-  }
-
-  initPWA(app)
-  function drawVideoOnCanvas(width = 500, height = 500) {
-    const canvas: HTMLCanvasElement = document.querySelector('#barcodeImg')!
-    const video: HTMLVideoElement = document.querySelector('#vid')!
-    const context = canvas.getContext("2d")!
-    canvas.width = width
-    canvas.height = height
-    const drawLoop = () => {
-      context.drawImage(video, 0, 0, width, height)
-      if (!paused) {
-        requestAnimationFrame(drawLoop)
-      }
-    }
-    requestAnimationFrame(drawLoop)
-  }
-  drawVideoOnCanvas()
-
-  let timeOut = 600
-  const barcodeDetectionLoop = () => {
-    setTimeout(() => {
-      detectBarcode()
-      timeOut = 600
-      barcodeDetectionLoop()
-    }, timeOut)
-  }
-  barcodeDetectionLoop()
 }
-main()
+
+function detectBarcode(fromImage = true) {
+  let imageEl: HTMLCanvasElement = document.querySelector("#barcodeImg")!
+  if (!fromImage) {
+    imageEl = document.querySelector("#vid")!
+  }
+
+  // check compatibility
+  if (!("BarcodeDetector" in globalThis)) {
+    console.log("Barcode Detector is not supported by this browser.")
+  } else {
+    // create new detector
+    // @ts-ignore
+    const barcodeDetector = new BarcodeDetector({
+      formats: [
+        "aztec",
+        "code_128",
+        "code_39",
+        "code_93",
+        "codabar",
+        "data_matrix",
+        "ean_13",
+        "ean_8",
+        "itf",
+        "pdf417",
+        "qr_code",
+        "upc_a",
+        "upc_e",
+        "unknown"
+      ],
+    })
+    barcodeDetector
+      .detect(imageEl)
+      .then((barcodes: any) => {
+        barcodes.forEach((barcode: any) => {
+          onBarcodeDetected(barcode)
+        })
+      })
+      .catch((err: any) => {
+        console.log(err)
+      })
+  }
+}
+
+const barcodeDetectionLoop = () => {
+  setTimeout(() => {
+    detectBarcode()
+    barcodeDetectionTick = 600
+    barcodeDetectionLoop()
+  }, barcodeDetectionTick)
+}
+
 
 function onBarcodeDetected(barcode: any) {
   paused = true
   const valueDiv = document.querySelector("#value")!
   valueDiv.textContent = "barcode value is:" + barcode.rawValue
-  paintBarcodeBoundingBox(barcode)
+  requestAnimationFrame(
+    () => paintBarcodeBoundingBox(barcode)
+  )
 }
 
 function paintBarcodeBoundingBox(barcode: any) {
-  console.log(barcode)
   const canvas: HTMLCanvasElement = document.querySelector('#barcodeImg')!
   const context = canvas.getContext("2d")!
   const bb = barcode.boundingBox
-  console.log("!!!!!", bb)
-  context.beginPath()
 
+  context.beginPath()
   context.strokeStyle = '#ff0000'
   context.rect(bb.x, bb.y, bb.width, bb.height)
   context.stroke()
-
-  // const data = context.getImageData(0, 0, width, height)
-  // const imageData = data.data
-
-  // for (let i = 0; i < imageData.length; i += 4) {
-  //   imageData[i] = 10000000
-  // }
-  // context.putImageData(data, 0, 0)
 }
+
+async function main() {
+  const app = document.querySelector<HTMLDivElement>('#app')!
+  initHTML()
+  initPWA(app)
+  initCamera()
+  initVideoElement()
+  drawVideoOnCanvas()
+  barcodeDetectionLoop()
+}
+
+main()
+
